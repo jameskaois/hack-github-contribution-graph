2725cd30e15ecd85f956f2e5235c308f51daa2f1
James Cao <cao.james069@gmail.com>
2023-01-01 12:00:00 +0700

    main.py before

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import git
 import os
+import datetime
 
 # Assuming 'my_repo' is the path to your repository
 repo_path = './'
 repo = git.Repo(repo_path)
+author = git.Actor("James Cao", "cao.james069@gmail.com")
 
 # Add a file to the staging area
 # For this example, let's create and add a file first
 with open(os.path.join(repo_path, 'example_file.txt'), 'w') as f:
-    f.write('Hello, GitPythonnn!')
+    f.write('Testing!')
 repo.index.add(['example_file.txt'])
 
-# Set the desired commit date in the format "YYYY-MM-DDTHH:MM:SS"
-date_string = "2025-07-26T12:00:00"
-os.environ['GIT_COMMITTER_DATE'] = date_string
+dates = [
+    datetime.datetime(2023, 1, 2, 12, 1, 0),
+]
 
 # Perform the commit with a message
-repo.index.commit("Test commit")
-
-# Optional: Clean up the environment variable
-del os.environ['GIT_COMMITTER_DATE']
+repo.index.commit("Testinggg", commit_date=dates[0].isoformat(), author=author)
 
 origin = repo.remote(name="origin")
 origin.push()
-
-# Trigger
