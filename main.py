9ab2adfe04459548e7ea4e4bd716dbf3cb8c6792
James Cao <cao.james069@gmail.com>
2025-08-09 14:45:07 +0700

    trigger github contribution

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import git
 import os
 
 # Assuming 'my_repo' is the path to your repository
-repo = git.Repo('my_repo')
+repo_path = './'
+repo = git.Repo(repo_path)
 
 # Add a file to the staging area
 # For this example, let's create and add a file first
-with open(os.path.join(repo.working_dir, 'example_file.txt'), 'w') as f:
-    f.write('Hello, GitPython!')
+with open(os.path.join(repo_path, 'example_file.txt'), 'w') as f:
+    f.write('Hello, GitPythonnn!')
 repo.index.add(['example_file.txt'])
 
 # Set the desired commit date in the format "YYYY-MM-DDTHH:MM:SS"
-date_string = "2023-01-01T12:00:00"
+date_string = "2025-07-26T12:00:00"
 os.environ['GIT_COMMITTER_DATE'] = date_string
 
 # Perform the commit with a message
-repo.index.commit("Initial commit")
+repo.index.commit("Test commit")
 
 # Optional: Clean up the environment variable
 del os.environ['GIT_COMMITTER_DATE']
 
-origin = repo.remote()
+origin = repo.remote(name="origin")
 origin.push()
+
+# Trigger
